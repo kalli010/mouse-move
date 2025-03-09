0ed8df12d4090363c1d6972b64bcc18dac11c530
zelkalai <zelkalai@c3r3p10.1337.ma>
2025-03-09 12:01:49 +0000

    update

--- ms.sh
+++ ms.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# if pgrep -f ms.sh > /dev/null; then
+#     nohup ./ms.sh > /dev/null 2>&1 &
+#     exit 0
+# fi
+
 while true; do
-	xdotool mousemove_relative 1 0
-	sleep 300
+  active_window=$(xdotool getactivewindow)
+  xdotool windowfocus xdotool getactivewindow > /dev/null 2>&1
+  xdotool type 'd' > /dev/null 2>&1
+  sleep 20
 done
